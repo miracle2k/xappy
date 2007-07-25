772af79e2c2f718071a5b396e0941f3ebc1c6603
boulton.rj <boulton.rj@eff16abe-6239-0410-8a55-6f1cd6cf4f8f>
2007-07-25 02:06:36 +0000

    secore/searchconnection.py: Correct small initialisation bug.
    git-svn-id: http://xappy.googlecode.com/svn/trunk@26 eff16abe-6239-0410-8a55-6f1cd6cf4f8f

--- libs/secore/secore/searchconnection.py
+++ libs/secore/secore/searchconnection.py
@@ -369,6 +369,7 @@ class SearchConnection(object):
         # class.  Move it to a shared location.
         config_file = _os.path.join(self._indexpath, 'config')
         if not _os.path.exists(config_file):
+            self._field_actions = {}
             self._field_mappings = _fieldmappings.FieldMappings()
             return
         fd = open(config_file)
