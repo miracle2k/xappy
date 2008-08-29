e6e563fa7e63d00d223c7ae0d4b6026e99a87fc5
boulton.rj <boulton.rj@eff16abe-6239-0410-8a55-6f1cd6cf4f8f>
2008-08-29 10:40:27 +0000

    utils/dump_field_actions.py: Add script to dump the field actions for a database. utils/replay_search_log.py: Correct copyright date.
    git-svn-id: http://xappy.googlecode.com/svn/trunk@286 eff16abe-6239-0410-8a55-6f1cd6cf4f8f

--- utils/dump_field_actions.py
+++ utils/dump_field_actions.py
@@ -15,45 +15,40 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-import time
 import xappy
 
-def display_time(starttime, count):
-    endtime = time.time()
-    print "%d,%.5f" % (count, endtime - starttime)
-
-def replay_from_file(conn, fd):
-    starttime = time.time()
-    count = 0
-    print "Searches,Total Time (seconds)"
-    for line in fd:
-        line = line.strip()
-        queryrepr, args, kwargs = eval(line)
-        query = conn.query_from_evalable(queryrepr)
-        results = query.search(*args, **kwargs)
-        count += 1
-        if count % 10 == 0:
-            display_time(starttime, count)
-    display_time(starttime, count)
-
+def dump_actions(conn):
+    actionnames = {
+        xappy.FieldActions.STORE_CONTENT: "STORE_CONTENT",
+        xappy.FieldActions.INDEX_EXACT: "INDEX_EXACT",
+        xappy.FieldActions.INDEX_FREETEXT: "INDEX_FREETEXT",
+        xappy.FieldActions.SORTABLE: "SORTABLE",
+        xappy.FieldActions.COLLAPSE: "COLLAPSE",
+        xappy.FieldActions.TAG: "TAG",
+        xappy.FieldActions.FACET: "FACET",
+        xappy.FieldActions.WEIGHT: "WEIGHT",
+        xappy.FieldActions.SORT_AND_COLLAPSE: "SORT_AND_COLLAPSE",
+    }
+    for field, actions in conn._field_actions.iteritems():
+        for actiontype, kwargslist in actions._actions.iteritems():
+            actionname = actionnames.get(actiontype)
+            for kwargs in kwargslist:
+                print "(%r, %s, %s)" % (field, actionname, repr(kwargs))
 
 usage = """
-replay_search_log.py <dbpath> <logpath>
+dump_field_actions.py <dbpath>
 """
 
 def run_from_commandline():
     import sys
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 2:
         print usage.strip()
         sys.exit(1)
 
     dbpath = sys.argv[1]
-    replayfile = sys.argv[2]
 
     conn = xappy.SearchConnection(dbpath)
-    fd = file(replayfile, "rb")
-    replay_from_file(conn, fd)
-    fd.close()
+    dump_actions(conn)
 
 if __name__ == "__main__":
     run_from_commandline()
