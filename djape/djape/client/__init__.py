e789c3c03df9352ac06d6062039e5d009e319f42
boulton.rj <boulton.rj@eff16abe-6239-0410-8a55-6f1cd6cf4f8f>
2008-12-16 12:41:42 +0000

    Move djape to its own repository.
    git-svn-id: http://xappy.googlecode.com/svn/trunk@331 eff16abe-6239-0410-8a55-6f1cd6cf4f8f

--- djape/djape/client/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from client import Client
-from document import Document
-from errors import SearchClientError
-from field import Field
-from query import Query, QueryPart, AllQuery, FreeTextQuery, GeoDistanceQuery
--- djape/djape/client/errors.py
+++
@@ -1,9 +0,0 @@
-
-class SearchClientError(Exception):
-    def __init__(self, msg, errtype):
-        self.msg = msg
-        self.errtype = errtype
-
-    def __str__(self):
-        return "%s: %s" % (self.errtype, self.msg)
-
