04212b19783d0027a70f46d5a40181c703824f8b
boulton.rj <boulton.rj@eff16abe-6239-0410-8a55-6f1cd6cf4f8f>
2009-03-06 05:48:31 +0000

    libs/build_xapian.sh,libs/get_xapian.py: Update with new tarballs, and add script to build and install (locally) all the tarballs.
    git-svn-id: http://xappy.googlecode.com/svn/trunk@407 eff16abe-6239-0410-8a55-6f1cd6cf4f8f

---
+++ libs/build_xapian.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+
+instdir="`pwd`/install/usr"
+pythonver=`python -c 'import sys;print sys.version[:3]'`
+
+mkdir build
+cd build
+mkdir xapian-core
+mkdir xapian-bindings
+mkdir xapian-extras
+mkdir xapian-extras-bindings
+
+cd xapian-core
+../../xapian-core/configure --prefix=$instdir
+make
+make install
+
+cd ../xapian-bindings
+../../xapian-bindings/configure --with-python --prefix=$instdir XAPIAN_CONFIG="$instdir/bin/xapian-config" PYTHON_LIB="$instdir/lib/python$pythonver/site-packages"
+make
+make install
+
+cd ../xapian-extras
+../../xapian-extras/configure --prefix=$instdir XAPIAN_CONFIG="$instdir/bin/xapian-config"
+make
+make install
+
+cd ../xapian-extras-bindings
+../../xapian-extras-bindings/configure --with-python --prefix=$instdir XAPIAN_CONFIG="$instdir/bin/xapian-config" PYTHON_LIB="$instdir/lib/python$pythonver/site-packages"
+make
+make install
