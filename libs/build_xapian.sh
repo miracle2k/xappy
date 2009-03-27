1b892d4b09744d00aa1df15df6d84b604ccd1e90
boulton.rj <boulton.rj@eff16abe-6239-0410-8a55-6f1cd6cf4f8f>
2009-03-27 11:03:19 +0000

    Update build script to use correct name for xapian-config instead of setting program-suffix.  Also allow python version to be specified by setting $PYTHON
    git-svn-id: http://xappy.googlecode.com/svn/trunk@423 eff16abe-6239-0410-8a55-6f1cd6cf4f8f

--- libs/build_xapian.sh
+++ libs/build_xapian.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -e
 
+PYTHON="${PYTHON=python}"
 instdir="`pwd`/install/usr"
-pythonver=`python -c 'import sys;print sys.version[:3]'`
+pythonver=`$PYTHON -c 'import sys;print sys.version[:3]'`
 
 mkdir build
 cd build
@@ -11,21 +12,21 @@ mkdir xapian-extras
 mkdir xapian-extras-bindings
 
 cd xapian-core
-../../xapian-core/configure --prefix=$instdir --program-suffix=
+../../xapian-core/configure --prefix=$instdir
 make
 make install
 
 cd ../xapian-bindings
-../../xapian-bindings/configure --with-python --prefix=$instdir XAPIAN_CONFIG="$instdir/bin/xapian-config" PYTHON_LIB="$instdir/lib/python$pythonver/site-packages"
+../../xapian-bindings/configure --with-python --prefix=$instdir XAPIAN_CONFIG="$instdir/bin/xapian-config-1.1" PYTHON="$PYTHON" PYTHON_LIB="$instdir/lib/python$pythonver/site-packages"
 make
 make install
 
 cd ../xapian-extras
-../../xapian-extras/configure --prefix=$instdir XAPIAN_CONFIG="$instdir/bin/xapian-config"
+../../xapian-extras/configure --prefix=$instdir XAPIAN_CONFIG="$instdir/bin/xapian-config-1.1"
 make
 make install
 
 cd ../xapian-extras-bindings
-../../xapian-extras-bindings/configure --with-python --prefix=$instdir XAPIAN_CONFIG="$instdir/bin/xapian-config" PYTHON_LIB="$instdir/lib/python$pythonver/site-packages"
+../../xapian-extras-bindings/configure --with-python --prefix=$instdir XAPIAN_CONFIG="$instdir/bin/xapian-config-1.1" PYTHON="$PYTHON" PYTHON_LIB="$instdir/lib/python$pythonver/site-packages"
 make
 make install
